7602d87a01e6da8f331d6f08d53e5a1d2b661d27
Selcia1708 <117474475+Selcia1708@users.noreply.github.com>
2026-03-01 17:50:52 +0000

    added migration

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -12,3 +12,8 @@ python manage.py migrate --noinput
 
 echo "==> Starting Daphne ASGI server"
 exec daphne -b 0.0.0.0 -p 8000 config.asgi:application
+
+#!/bin/bash
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+exec "$@"
